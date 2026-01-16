97685352ca6083f5133f6791a729eb9e3b6398f3
foryouchannelofficial <foryouchannelofficial@gmail.com>
2026-01-16 03:18:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const cors_proxy = require('cors-anywhere');
-const port = process.env.PORT || 3000;
 
 cors_proxy.createServer({
     originWhitelist: [], // Bárki elérheti
-    removeHeaders: ['x-frame-options', 'content-security-policy']
-}).listen(port, '0.0.0.0', () => {
-    console.log('A proxy fut a ' + port + ' porton');
+    requireHeader: [],   // Nem várunk el speciális fejlécet a klienstől
+    removeHeaders: [
+        'x-frame-options',
+        'content-security-policy',
+        'set-cookie',
+        'expect-ct'
+    ],
+    setHeaders: {
+        // Elhitetjük a TV2-vel, hogy tőle jön a kérés
+        'origin': 'https://tv2play.hu',
+        'referer': 'https://tv2play.hu',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+    }
+}).listen(3000, '0.0.0.0', () => {
+    console.log('Emelt szintű proxy fut...');
 });
